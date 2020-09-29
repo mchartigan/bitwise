71cac2fcb21053bb9f2344bc4bad7c746552bdf6
zgeorge22 <zgeorge0123@gmail.com>
2020-09-29 17:51:02 -0400

    Give proper form validation errors when saving profile changes

--- public/app.js
+++ public/app.js
@@ -1,15 +1,37 @@
+$('form').form({
+    on: 'blur',
+    fields: {
+        username: {
+            identifier: 'username',
+            rules: [{
+                type: 'empty',
+                prompt: 'Please enter a username'
+            }]
+        },
+        file: {
+            identifier: 'file',
+            rules: [{
+                type: 'empty',
+                prompt: 'Please choose a file'
+            }]
+        }
+    }
+});
+
 const UID = '9fvVztcaIGAn5mxGODAE';
 
 //const form = document.querySelector('#account-info-form');
 let username = document.getElementById('username'),
     email = document.getElementById('email')
     bio = document.getElementById('bio'),
-    img = document.getElementById('img');
+    img = document.getElementById('img'),
+    user_err = document.getElementById('username-error');
 
 db.collection("users").doc(UID).onSnapshot(function(doc) {
     username.value = doc.data().username;
     email.value = doc.data().email;
     bio.value = doc.data().bio;
+
     firebase.storage().ref('usercontent/' + UID + '/profile.jpg').getDownloadURL().then(imgURL => {
         img.src = imgURL;
     })
@@ -28,10 +50,12 @@ function saveButtonPressed() {
         bio: bio.value,
     })
 
-    firebase.storage().ref('usercontent/' + UID + '/profile.jpg').put(file).then(function () {
-        console.log('Successfully Uploaded')
-        firebase.storage().ref('usercontent/' + UID + '/profile.jpg').getDownloadURL().then(imgURL => {
-            img.src = imgURL;
+    if (file != null) {
+        firebase.storage().ref('usercontent/' + UID + '/profile.jpg').put(file).then(function () {
+            // console.log('Successfully Uploaded')
+            firebase.storage().ref('usercontent/' + UID + '/profile.jpg').getDownloadURL().then(imgURL => {
+                img.src = imgURL;
+            })
         })
-    })
+    }
 }
