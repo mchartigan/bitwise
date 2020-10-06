6e7e28732c44ccc0ab4b2ebff795b626c4f957d4
mchartigan <hartigan@purdue.edu>
2020-10-06 15:15:31 -0400

    test initial image attachment functionality

--- public/js/create_post.js
+++ public/js/create_post.js
@@ -1,9 +1,15 @@
-const postForm = document.querySelector('#create-post-form');
+var UID = null;
+var storage = firebase.storage();
+var storageRef = storage.ref();
+var docRef = null;
 
 let titleField = document.getElementById('title-field'),
     bodyField  = document.getElementById('body-field'),
     topicField = document.getElementById('topic-field'),
-    anonBox    = document.getElementById('anon-checkbox');
+    anonBox    = document.getElementById('anon-checkbox'),
+    imageField = document.getElementById('image-file-field'),
+    hasImage   = false,
+    imageFile  = {};
 
 function submitPost() {
     // check topic -- can be performed asynchronously
@@ -55,6 +61,7 @@ function addPost(auth, uid, title, body, subject) {
         authoruid: uid,
         title: title,
         content: body,
+        hasImage: false,
         embed: null,
         created: new Date(),
         parent: null,
@@ -63,13 +70,27 @@ function addPost(auth, uid, title, body, subject) {
         upvotes: [null],
         downvotes: [null]
     }).then(reference => {
+
+        if (imageField.files.length != 0) {
+            // Update storage with new image file
+            var path = 'posts/' + reference.id + '/1.jpg';
+            storageRef.child(path).put(imageFile).then(function () {
+                console.log('Successfully Uploaded: ', path); // DEBUG LOG
+    
+                reference.set({
+                    hasImage: true
+                },{merge: true})
+            }).catch(err => {
+                console.log('Failed to Upload: ', path); // DEBUG LOG
+            });
+        }
         if(reference) {
             titleField.value = '';
             bodyField.value  = '';
             topicField.value = '';
+            removeImage();
 
-
-            location.replace("/index.html");
+            //location.replace("/index.html");
         }
     });
 }
@@ -78,6 +99,24 @@ function cancelPost() {
     titleField.value = '';
     bodyField.value  = '';
     topicField.value = '';
+}
+
+function uploadImage(e) {
+    imageFile = e.target.files[0];
+    hasImage = true;
+
+    // Stage chosen image file
+    //previewImage();
+}
+
+function removeImage() {
+    // Stage remove image change
+    if (hasImage) {
+        imageField.value = '';
+        imageFile = {};
+        hasImage = false;
+        //previewImage();
+    }
 }
 
 // anonymous toggle box
