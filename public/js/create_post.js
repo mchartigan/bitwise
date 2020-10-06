0ea81c7ac2e05b87adbf3fbfe8b87124e23f496d
mchartigan <hartigan@purdue.edu>
2020-10-06 16:46:39 -0400

    clean up and fix header login issues

--- public/js/create_post.js
+++ public/js/create_post.js
@@ -8,8 +8,54 @@ let titleField = document.getElementById('title-field'),
     topicField = document.getElementById('topic-field'),
     anonBox    = document.getElementById('anon-checkbox'),
     imageField = document.getElementById('image-file-field'),
-    hasImage   = false,
+    postImage  = document.getElementById('post-image'),
     imageFile  = {};
+
+// initially hide this button
+$('#signin-button').hide();
+
+// load Bitwise logo in menu bar
+storageRef.child('assets/logo.png').getDownloadURL().then(imgURL => {
+    $('#logo-icon').attr('src', imgURL);
+    console.log('Successfully Downloaded Logo Icon'); // DEBUG LOG
+}).catch(err => {
+    console.log('Failed to Download Icon'); // DEBUG LOG
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+
+        UID = user.uid;
+        console.log('Retrieved UID');
+
+        docRef = db.collection("users").doc(UID);
+
+        docRef.get().then(function(doc) {
+            loadProfileIcon(doc);
+            document.getElementById('account-dropdown').innerHTML = '&nbsp; ' + doc.data().username;
+        });
+    } else {
+        
+        // hide dropdown and replace with a signin button
+        $('#parent-dropdown-item').hide();
+        $('#signin-button').show();
+    }
+});
+
+function loadProfileIcon(doc) {
+    if (doc.data().picFlag) {
+        path = 'usercontent/' + UID + '/profile.jpg';
+    } else {
+        path = 'usercontent/default/profile.jpg';
+    }
+
+    storageRef.child(path).getDownloadURL().then(imgURL => {
+        $('#profile-icon').attr('src', imgURL);
+        console.log('Successfully Downloaded Profile Icon'); // DEBUG LOG
+    }).catch(err => {
+        console.log('Failed to Download Profile Icon'); // DEBUG LOG
+    });
+}
 
 function submitPost() {
     // check topic -- can be performed asynchronously
@@ -28,10 +74,10 @@ function submitPost() {
     }
 
     // check username / UID
-    var uid = firebase.auth().currentUser.uid;
+    UID = firebase.auth().currentUser.uid;
     // if user chose to post anonymously or isn't signed in, post anonymously
     let anon = $("input[name='anonymous']:checked").val();
-    if (anon || !uid) {
+    if (anon || !UID) {
         addPost(
             auth=null,
             uid=null,
@@ -42,10 +88,10 @@ function submitPost() {
     }
     // otherwise post with username attached
     else {
-        db.collection('users').doc(uid).get().then(user => {
+        db.collection('users').doc(UID).get().then(user => {
             addPost(
                 auth=user.data().username,
-                uid=uid,
+                uid=UID,
                 title=titleField.value,
                 body=bodyField.value,
                 subject=topic
@@ -61,7 +107,7 @@ function addPost(auth, uid, title, body, subject) {
         authoruid: uid,
         title: title,
         content: body,
-        hasImage: false,
+        image: null,
         embed: null,
         created: new Date(),
         parent: null,
@@ -70,27 +116,32 @@ function addPost(auth, uid, title, body, subject) {
         upvotes: [null],
         downvotes: [null]
     }).then(reference => {
-
-        if (imageField.files.length != 0) {
-            // Update storage with new image file
-            var path = 'posts/' + reference.id + '/1.jpg';
-            storageRef.child(path).put(imageFile).then(function () {
-                console.log('Successfully Uploaded: ', path); // DEBUG LOG
-    
-                reference.set({
-                    hasImage: true
-                },{merge: true})
-            }).catch(err => {
-                console.log('Failed to Upload: ', path); // DEBUG LOG
-            });
-        }
         if(reference) {
-            titleField.value = '';
-            bodyField.value  = '';
-            topicField.value = '';
-            removeImage();
+            if (imageField.files.length != 0) {
+                // Update storage with new image file
+                var path = `posts/${reference.id}/1.jpg`;
+                storageRef.child(path).put(imageFile).then(function (upload) {
+                    
+                    // get download URL for image and attach to post
+                    return upload.ref.getDownloadURL();
+                }).then(function(downloadURL) {
 
-            //location.replace("/index.html");
+                    reference.set({
+                        image: downloadURL
+                    },{merge: true});
+
+                    cancelPost();
+
+                    console.log('Successfully Uploaded: ', path); // DEBUG LOG
+                    location.replace("/index.html");
+                }).catch(err => {
+                    console.log('Failed to Upload: ', path); // DEBUG LOG
+                });
+            }
+            else {
+                cancelPost();
+                location.replace("/index.html");
+            }
         }
     });
 }
@@ -99,23 +150,41 @@ function cancelPost() {
     titleField.value = '';
     bodyField.value  = '';
     topicField.value = '';
+    removeImage();
+}
+
+function previewImage() {
+ 
+    if (imageField.files.length != 0) {
+        // Display chosen image file
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            postImage.src = e.target.result;
+        };
+        reader.readAsDataURL(imageFile);
+
+        $('#post-image').show();
+    }
+    else {
+        $('#post-image').hide();
+        postImage.src = '//:0';
+    }
 }
 
 function uploadImage(e) {
     imageFile = e.target.files[0];
-    hasImage = true;
 
     // Stage chosen image file
-    //previewImage();
+    previewImage();
 }
 
 function removeImage() {
     // Stage remove image change
-    if (hasImage) {
+    if (imageField.files.length != 0) {
         imageField.value = '';
         imageFile = {};
-        hasImage = false;
-        //previewImage();
+
+        previewImage();
     }
 }
 
