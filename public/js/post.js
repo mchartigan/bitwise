993234dcb2f6744d489a69de152c8a7d9fe4cee8
zgeorge22 <zgeorge0123@gmail.com>
2020-10-21 16:49:51 -0400

    Update a basic test feed to use react components and semantic ui "comment" styling

---
+++ public/js/post.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class Post extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { userRetrieved: false };
+
+        this.authorUsername = "loading...";
+        this.authorImageURL = "https://firebasestorage.googleapis.com/v0/b/bitwise-a3c2d.appspot.com/o/usercontent%2Fdefault%2Fprofile.jpg?alt=media&token=f35c1c16-d557-4b94-b5f0-a1782869b551";
+        this.createdText = this.props.created;
+        if (this.props.topic != "") {
+            this.titleText = "["+this.props.topic+"] "+this.props.title;
+        } else {
+            this.titleText = this.props.title;
+        }
+        this.contentText = this.props.content;
+        if (this.props.imageURL != null) {
+            this.imageURL = this.props.imageURL;
+        }
+
+        db.collection("users").doc(this.props.authorUID).get().then((doc) => {
+            this.authorUsername = doc.data().username,
+            this.authorImageURL = doc.data().profileImageURL
+            // Re-render post
+            this.setState({userRetrieved: true});
+        });
+    }
+
+    // Format post
+    render() {
+        return (
+            <div className="comment">
+                <a className="avatar">
+                    <img src={this.authorImageURL}></img>
+                </a>
+                <div className="content">
+                    <a className="author">{this.authorUsername}</a>
+                    <div className="metadata">
+                        <span className="date">{this.createdText}</span>
+                    </div>
+                    <div className="text">
+                        <div className="ui medium header">{this.titleText}</div>
+                    </div>
+                    <div className="text">
+                        {this.contentText}
+                    </div>
+                    <div className="actions">
+                        <a className="reply">Reply</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ public/js/test.js
@@ -0,0 +1,67 @@
+var UID = null;
+var storage = firebase.storage();
+var storageRef = storage.ref();
+var docRef = null;
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        UID = user.uid;
+        docRef = db.collection("users").doc(UID);
+
+        loadDropdown();
+
+        $("#login-button").hide();
+        $("#user-dropdown").show();
+        $("#create-post-button").show();
+
+        loadPosts();
+    } else {
+        $("#login-button").show();
+        $("#user-dropdown").hide();
+        $("#create-post-button").hide();
+    }
+});
+
+function loadDropdown() {
+    docRef.get().then(function (doc) {
+        $('#profile-icon').attr('src', doc.data().profileImageURL);
+        document.getElementById('account-dropdown').innerHTML = '&nbsp; ' + doc.data().username;
+    });
+}
+
+// Loads all of the logged in users posts
+function loadPosts() {
+    db.collection('posts')
+        .where('authoruid', 'in', [UID])
+        .orderBy('created', 'desc')
+        .get().then(function (querySnapshot) {
+            if (querySnapshot.empty) {
+                // Display error message
+                ReactDOM.render(<div className="ui red message">No Posts Available!</div>, document.querySelector('#feed'));
+            } else {
+                // Threaded post container
+                ReactDOM.render(<div className="ui threaded comments">
+                                    <div id="posts"></div>
+                                </div>,
+                                document.querySelector('#feed'));
+
+                const posts = [];
+
+                // Loop through each post to add formatted JSX element to list
+                querySnapshot.forEach(doc => {
+                    const postProps = {
+                        authorUID: doc.data().authoruid,
+                        created: doc.data().created.toDate().toString(),
+                        topic: doc.data().topic,
+                        title: doc.data().title,
+                        content: doc.data().content,
+                        imgURL: doc.data().image
+                    };
+
+                    posts.push(<Post {...postProps} key={doc.id}/>);
+                });
+
+                ReactDOM.render(posts, document.querySelector('#posts'));
+            }
+        });
+}
