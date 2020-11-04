e8130d08b2e0ec7458553ee22f3dc6e065b9ab9e
Zachary J McClary <zmcclary@purdue.edu>
2020-11-04 00:28:30 -0500

    Follow Button Updates More

--- public/js/topic.js
+++ public/js/topic.js
@@ -1,4 +1,5 @@
 var UID = null;
+var topicname = null;
 var storage = firebase.storage();
 var storageRef = storage.ref();
 var docRef = null;
@@ -30,7 +31,6 @@ function loadDropdown() {
 
 // Loads all of the logged in users posts
 function loadPosts() {
-    var topicname = window.location.href.split('/')[4];
     db.collection('posts')
         .orderBy('created', 'desc')
         .where('topic','==',topicname)
@@ -62,9 +62,42 @@ function loadPosts() {
             }
         });
 }
+function loadFollowButton() {
+    // IN PROGRESS
+    db.collection('users').doc(UID).get().then(doc => {
+        var loc = doc.data().followingTopics.indexOf(topicname);
+        if (loc > -1) {
+            // they are currently following
+            followState = true;
+            $('#follow-button').innerHTML("Unfollow")
+        } else {
+            followState = false;
+            $('#follow-button').innerHTML("Follow")
+        }
+        $('#follow-button').show();
+    });
+}
+
+function changeFollowState() {
+    // check current user follow list
+    console.log("changing state");
+    db.collection('users').doc(UID).get().then(doc => {
+        var loc = doc.data().followingUsers.indexOf(viewUID);
+        if (loc > -1) {
+            // they are currently following. unfollow.
+            db.collection('users').doc(UID).update({
+                followingUsers: firebase.firestore.FieldValue.arrayRemove(viewUID)
+            });
+        } else {
+            db.collection('users').doc(UID).update({
+                followingUsers: firebase.firestore.FieldValue.arrayUnion(viewUID)
+            });
+        }
+    });
+    loadFollowButton();
+}
 
 function loadTopic() {
-    var pagename = window.location.href.split('/')[4];
-    $("#topic-name").html(pagename);
-    console.log(pagename);
+    topicname = window.location.href.split('/')[4];
+    $("#topic-name").html(topic);
 }
