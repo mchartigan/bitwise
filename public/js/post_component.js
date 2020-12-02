997f053b70497d9f7d1050c8ccba220ee59cf3bc
zgeorge22 <zgeorge0123@gmail.com>
2020-12-01 22:23:47 -0500

    Add mobile resizing funcitonality when a breakpoint threshold is reached

--- public/js/post_component.js
+++ public/js/post_component.js
@@ -438,7 +438,6 @@ class Post extends React.Component {
                                         onClick={(event) => {
                                             event.target.onselectstart = function () { return false; };
                                             this.setState({ confirmDelete: true });
-                                            setTimeout(() => { this.setState({ confirmDelete: false }); }, 3000);
                                         }}>
                                         <i className="trash alternate outline icon" ></i>
                                     </a>
