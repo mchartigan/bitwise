d11e1abaf1236fa0347b4c275a2d2d7293ea9e31
zgeorge22 <zgeorge0123@gmail.com>
2020-12-01 23:06:46 -0500

    Fix account deletion button bug from last merge

--- public/js/account.js
+++ public/js/account.js
@@ -5,10 +5,8 @@ var imageFile = {}
 var curProfileImageURL = "https://firebasestorage.googleapis.com/v0/b/bitwise-a3c2d.appspot.com/o/usercontent%2Fdefault%2Fprofile.jpg?alt=media&token=f35c1c16-d557-4b94-b5f0-a1782869b551";
 
 firebase.auth().onAuthStateChanged(function (user) {
-    $("#warning-text").hide();
-    $("#accept-delete").hide();
-    $("#deny-delete").hide();
     UID = user ? user.uid : null;
+
     loadPage().then(() => {
         pageMounted();
         if (UID) {
@@ -29,153 +27,158 @@ function Page(props) {
 
     return (
         <div className='ui main text container'>
-        <div className="ui stackable two column grid">
-            <div className="four wide column">
-                <div className={"ui secondary" + dark + "vertical pointing fluid menu"}>
-                    <a className={accent + "item active"} data-tab="information">
-                        <i className="user icon"></i>
+            <div className="ui stackable two column grid">
+                <div className="four wide column">
+                    <div className={"ui secondary" + dark + "vertical pointing fluid menu"}>
+                        <a className={accent + "item active"} data-tab="information">
+                            <i className="user icon"></i>
                         Information
                     </a>
 
-                    <a className={accent + "item"} data-tab="display">
-                        <i className="desktop icon"></i>
+                        <a className={accent + "item"} data-tab="display">
+                            <i className="desktop icon"></i>
                         Display
                     </a>
 
-                    <a className={accent + "item"} data-tab="accessibility">
-                        <i className="universal access icon"></i>
+                        <a className={accent + "item"} data-tab="accessibility">
+                            <i className="universal access icon"></i>
                         Accessibility
                     </a>
 
-                    <a className={accent + "item"} data-tab="options">
-                        <i className="sliders horizontal icon"></i>
+                        <a className={accent + "item"} data-tab="options">
+                            <i className="sliders horizontal icon"></i>
                         Options
                     </a>
+                    </div>
                 </div>
-            </div>
 
-            <div className="twelve wide stretched column">
-                <div className={"ui" + dark + "tab segment active"} data-tab="information">
-                    <form className={'ui' + dark + 'form'} id='account-info'>
-                        <div className='field'>
-                            <label>Profile Picture</label>
-                        </div>
+                <div className="twelve wide stretched column">
+                    <div className={"ui" + dark + "tab segment active"} data-tab="information">
+                        <form className={'ui' + dark + 'form'} id='account-info'>
+                            <div className='field'>
+                                <label>Profile Picture</label>
+                            </div>
 
-                        <img className='ui medium bordered image' id='profile-image'
-                            src="https://firebasestorage.googleapis.com/v0/b/bitwise-a3c2d.appspot.com/o/usercontent%2Fdefault%2Fprofile.jpg?alt=media&token=f35c1c16-d557-4b94-b5f0-a1782869b551" />
-                        <br />
+                            <img className='ui medium bordered image' id='profile-image'
+                                src="https://firebasestorage.googleapis.com/v0/b/bitwise-a3c2d.appspot.com/o/usercontent%2Fdefault%2Fprofile.jpg?alt=media&token=f35c1c16-d557-4b94-b5f0-a1782869b551" />
+                            <br />
 
-                        <label className={"ui" + dark + "basic button"} htmlFor="image-file-field">
-                            <i className="file icon"></i>
+                            <label className={"ui" + dark + "basic button"} htmlFor="image-file-field">
+                                <i className="file icon"></i>
                             Upload
                         </label>
-                        <input type="file" accept=".png, .jpg, .jpeg" id="image-file-field" style={{display: "none"}}
-                            onChange={e => uploadImage(e)} />
-                        <div className={"ui" + dark + "basic button"} onClick={removeImage}>
-                            <i className="trash icon"></i>
+                            <input type="file" accept=".png, .jpg, .jpeg" id="image-file-field" style={{ display: "none" }}
+                                onChange={e => uploadImage(e)} />
+                            <div className={"ui" + dark + "basic button"} onClick={removeImage}>
+                                <i className="trash icon"></i>
                             Remove
                         </div>
-                        <br /><br />
+                            <br /><br />
 
-                        <div className='field'>
-                            <label>Username</label>
-                            <input type="text" id="username-field" required />
-                        </div>
-
-                        <div className='field'>
-                            <label>Short Bio</label>
-                            <textarea rows="2" type="text" id="bio-txt-field" onKeyUp={e => countChars(e.target)}></textarea>
-                          <p id="charNum"></p>
-                        </div>
-
-                        <div className='field'>
-                            <label>Email</label>
-                            <div className="ui disabled input">
-                                <input type="text" placeholder="retrieving email..." id="email-field" />
+                            <div className='field'>
+                                <label>Username</label>
+                                <input type="text" id="username-field" required />
                             </div>
-                        </div>
+
+                            <div className='field'>
+                                <label>Short Bio</label>
+                                <textarea rows="2" type="text" id="bio-txt-field" onKeyUp={e => countChars(e.target)}></textarea>
+                                <p id="charNum"></p>
+                            </div>
+
+                            <div className='field'>
+                                <label>Email</label>
+                                <div className="ui disabled input">
+                                    <input type="text" placeholder="retrieving email..." id="email-field" />
+                                </div>
+                            </div>
+                            <br />
+
+                            <div className={'ui' + accent + 'submit button'}>Save</div>
+                            <div className={'ui' + dark + 'basic button'} onClick={cancelForm}>Cancel</div>
+                            <div className="ui error message"></div>
+                        </form>
+                    </div>
+
+                    <div className={"ui" + dark + "tab segment"} data-tab="display">
+                        Dark Mode
                         <br />
-
-                        <div className={'ui' + accent + 'submit button'}>Save</div>
-                        <div className={'ui' + dark + 'basic button'} onClick={cancelForm}>Cancel</div>
-                        <div className="ui error message"></div>
-                    </form>
-                </div>
-
-                <div className={"ui" + dark + "tab segment"} data-tab="display">
-                    Dark Mode
-                    <br />
                         <div className={"ui toggle checkbox"} id="dark-value">
                             <input type="checkbox" />
                             <label></label>
                         </div>
-                    <br />
-                    <br />
-                    Accent Color
-                    <br />
-                    <div className="ui selection dropdown" id="accent-value">
-                        <input type="hidden" />
-                        <i className="dropdown icon"></i>
-                        <div className="default text">Default</div>
-                        <div className ="scrollhint menu">
-                            <div className="item" data-value="red">
-                                <div className="ui red empty circular label"></div>
+                        <br />
+                        <br />
+                        Accent Color
+                        <br />
+                        <div className="ui selection dropdown" id="accent-value">
+                            <input type="hidden" />
+                            <i className="dropdown icon"></i>
+                            <div className="default text">Default</div>
+                            <div className="scrollhint menu">
+                                <div className="item" data-value="red">
+                                    <div className="ui red empty circular label"></div>
                                 Red
                             </div>
-                            <div className="item" data-value="orange">
-                                <div className="ui orange empty circular label"></div>
+                                <div className="item" data-value="orange">
+                                    <div className="ui orange empty circular label"></div>
                                 Orange
                             </div><div className="item" data-value="yellow">
-                                <div className="ui yellow empty circular label"></div>
+                                    <div className="ui yellow empty circular label"></div>
                                 Yellow
                             </div>
-                            <div className="item" data-value="olive">
-                                <div className="ui olive empty circular label"></div>
+                                <div className="item" data-value="olive">
+                                    <div className="ui olive empty circular label"></div>
                                 Olive
                             </div><div className="item" data-value="green">
-                                <div className="ui green empty circular label"></div>
+                                    <div className="ui green empty circular label"></div>
                                 Green
                             </div>
-                            <div className="item" data-value="teal">
-                                <div className="ui teal empty circular label"></div>
+                                <div className="item" data-value="teal">
+                                    <div className="ui teal empty circular label"></div>
                                 Teal
                             </div><div className="item" data-value="blue">
-                                <div className="ui blue empty circular label"></div>
+                                    <div className="ui blue empty circular label"></div>
                                 Blue
                             </div>
-                            <div className="item" data-value="violet">
-                                <div className="ui violet empty circular label"></div>
+                                <div className="item" data-value="violet">
+                                    <div className="ui violet empty circular label"></div>
                                 Violet
                             </div><div className="item" data-value="purple">
-                                <div className="ui purple empty circular label"></div>
+                                    <div className="ui purple empty circular label"></div>
                                 Purple
                             </div>
-                            <div className="item" data-value="pink">
-                                <div className="ui pink empty circular label"></div>
+                                <div className="item" data-value="pink">
+                                    <div className="ui pink empty circular label"></div>
                                 Pink
                             </div>
-                            <div className="item" data-value="brown">
-                                <div className="ui brown empty circular label"></div>
+                                <div className="item" data-value="brown">
+                                    <div className="ui brown empty circular label"></div>
                                 Brown
                             </div>
+                            </div>
                         </div>
+                        <br />
+                        <br />
+                        <div className={'ui' + accent + 'button'} onClick={saveTheme}>Save</div>
+                        <div className={'ui' + dark + 'basic button'} onClick={cancelTheme}>Cancel</div>
                     </div>
-                    <br />
-                    <br />
-                    <div className={'ui' + accent + 'button'} onClick={saveTheme}>Save</div>
-                    <div className={'ui' + dark + 'basic button'} onClick={cancelTheme}>Cancel</div>
+
+                    <div className={"ui" + dark + "tab segment"} data-tab="accessibility">
+                        Enable Accessibility Features
                 </div>
 
-                <div className={"ui" + dark + "tab segment"} data-tab="accessibility">
-                    Enable Accessibility Features
-                </div>
-
-                <div className={"ui" + dark + "tab segment"} data-tab="options">
-                    Delete Account
+                    <div className={"ui" + dark + "tab segment"} data-tab="options">
+                        <div class='ui red button' id='delete-button' onClick={deleteWarning}>Delete Account</div>
+                        <label id="warning-text">WARNING: This action cannot be reverted. Are you sure you want to delete your account?</label>
+                        <br />
+                        <br />
+                        <div class='ui red button' id='accept-delete' onclick={deleteAccount}>Yes</div>
+                        <div class='ui button' id='deny-delete' onClick={deleteBackout}>No</div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
@@ -185,14 +188,14 @@ function loadPage() {
             // Load dropdown
             db.collection("users").doc(UID).get().then(function (doc) {
                 var dark = doc.data().dark;
-                var accent= doc.data().accent;
+                var accent = doc.data().accent;
                 background(dark, accent);
-                ReactDOM.render(<Page accent={accent} dark={dark}/>, document.getElementById("page"));
+                ReactDOM.render(<Page accent={accent} dark={dark} />, document.getElementById("page"));
                 resolve();
             });
         } else {
             background(false, "violet");
-            ReactDOM.render(<Page accent="violet" dark={false}/>, document.getElementById("page"));
+            ReactDOM.render(<Page accent="violet" dark={false} />, document.getElementById("page"));
             resolve();
         }
     })
@@ -283,6 +286,16 @@ function cancelForm() {
     $('.ui.form').form('reset');
 }
 
+function countChars(obj) {
+    var maxLength = 160;
+    var strLength = obj.value.length;
+    if (strLength > maxLength) {
+        document.getElementById("charNum").innerHTML = '<span style = "color: red;">' + strLength + '/' + maxLength + ' characters</span>';
+    } else {
+        document.getElementById("charNum").innerHTML = strLength + '/' + maxLength + ' characters';
+    }
+}
+
 function cancelTheme() {
     // Reset theme values with original data in firestore
     loadTheme();
@@ -309,17 +322,11 @@ function saveTheme() {
     })
 }
 
-function countChars(obj) {
-    var maxLength = 160;
-    var strLength = obj.value.length;
-    if (strLength > maxLength) {
-        document.getElementById("charNum").innerHTML = '<span style = "color: red;">' + strLength + '/' + maxLength + ' characters</span>';
-    } else {
-        document.getElementById("charNum").innerHTML = strLength + '/' + maxLength + ' characters';
-    }
-}
-
 function pageMounted() {
+    $("#warning-text").hide();
+    $("#accept-delete").hide();
+    $("#deny-delete").hide();
+
     // Initialize dynamic tab groups
     $('.menu .item').tab();
     // Initialize accent color dropdown menu
@@ -381,11 +388,11 @@ function checkDuplicates(param) {
 }
 
 function deleteWarning() {
-  //Show hidden warning buttons and hide the original button
-  $("#delete-button").hide();
-  $("#warning-text").show();
-  $("#accept-delete").show();
-  $("#deny-delete").show();
+    //Show hidden warning buttons and hide the original button
+    $("#delete-button").hide();
+    $("#warning-text").show();
+    $("#accept-delete").show();
+    $("#deny-delete").show();
 }
 
 function deleteAccount() {
@@ -411,15 +418,15 @@ function deleteAccount() {
                 var user = firebase.auth().currentUser;
                 //Delete the user document from the database
                 db.collection('users').doc(UID).delete();
-        
+
                 //Officially delete user from database and redirect to index
-                user.delete().then(function() {
+                user.delete().then(function () {
                     // Something
-                }).catch(function(error) {
+                }).catch(function (error) {
                     // An error happened.
                 });
             }
-    });
+        });
 }
 
 async function callAsyncDeletePost(postId, numPost, totalPost) {
@@ -431,9 +438,9 @@ async function callAsyncDeletePost(postId, numPost, totalPost) {
         db.collection('users').doc(UID).delete();
 
         //Officially delete user from database and redirect to index
-        user.delete().then(function() {
+        user.delete().then(function () {
             // Something
-        }).catch(function(error) {
+        }).catch(function (error) {
             // An error happened.
         });
     }
@@ -501,7 +508,7 @@ async function deletePost(postID) {
 
             db.collection('posts').doc(postID).delete();
         });
-    } catch(err) {
+    } catch (err) {
         return;
     }
 }
